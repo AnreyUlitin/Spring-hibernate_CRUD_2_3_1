45924d8ecab41ce6aefbd372b597e67ac0372aa1
Andrey <anulitin@rambler.ru>
2021-11-12 08:31:56 +0300

    Spring-hibernate_2_3_1

--- src/main/java/website/service/UserService.java
+++ src/main/java/website/service/UserService.java
@@ -6,14 +6,14 @@ import java.util.List;
 public interface UserService {
 
     List<User> getAllUsers();
+
     User getUserById(Long id);
+
     void addUser(User user);
+
     void updateUser(User user);
 
     void deleteUserById(Long id);
-
-
-
 
 }
 
